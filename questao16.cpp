fe8b08a9858539ba4a721bf78be063c039cad08f
GiovanaAndrade1 <ferreirapoc@gmail.com>
2025-07-21 10:56:23 -0300

    .

---
+++ questao16.cpp
@@ -0,0 +1,15 @@
+/*16. Escreva um algoritmo em C++ que leia um conjunto de 50 fichas de funcionários de uma empresa e armazene-as em vetores. Cada ficha deve conter o nome, o salário e o código do setor (um número de 1 a 5).
+ O algoritmo deve calcular e imprimir:
+    1. O maior e o menor salário da empresa.
+    2. A média salarial de cada setor.
+    3. Os funcionários com salários abaixo da média salarial da empresa.*/
+
+
+    int ficha;
+
+
+    int main(){
+        int *vetor = new int [5];
+
+        return 0;
+    }
