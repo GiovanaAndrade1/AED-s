11af99dba43ce04b1e3b5051a0e872ed6cb3a437
GiovanaAndrade1 <ferreirapoc@gmail.com>
2025-07-21 13:43:10 -0300

    questão incompleta

--- questao16.cpp
+++ questao16.cpp
@@ -4,12 +4,39 @@
     2. A média salarial de cada setor.
     3. Os funcionários com salários abaixo da média salarial da empresa.*/
 
+#include <string>
+#include <iostream> 
+using namespace std;
 
-    int ficha;
+int leituraDados(string *nome, float *salario, int *cod){
+    int codigo = 0;
+    for (int i = 0 ; i < 5 ; i ++){
+        cout << "Informe o nome da "<< i + 1 << "Ficha: "<< endl;
+        cin >> nome[i];
+        cout << " Informe o salario da "<< i + 1 << "Ficha: " << endl;
+        cin >> salario[i];
+        cout << " Informe o código da "<< i+ 1 << "ficha: " << endl;
+        cin >> codigo;
+        while ( codigo < 1 || codigo > 5){
+            cout << " tente de novo,  numero de 1 a 5 "<< endl;
+            cin >> codigo;
+        }
+        cod[i] = codigo;
+
+        }
+
+return 0;
+    }
+    
 
 
     int main(){
-        int *vetor = new int [5];
+        string *nome = new string[5];
+        float *salario = new float[5];
+        int *cod = new int[5];
+
+
+        
 
         return 0;
     }
